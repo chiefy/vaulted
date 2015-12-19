ca22cfed543ebd382eb4522c218e3dfc96a327c1
kenjones <kenjones@cisco.com>
2015-12-19 00:41:15 -0500

    add lint and test setup to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,34 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var gulpJsdoc2md = require('gulp-jsdoc-to-markdown');
 var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 
 
 var SOURCE_CODE = ['lib/**/*.js'];
+var TEST_CODE = ['tests/**/*.js'];
+
+gulp.task('lint', function() {
+  return gulp.src(SOURCE_CODE)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('pre-test', ['lint'], function () {
+  return gulp.src(SOURCE_CODE)
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(TEST_CODE)
+    .pipe(mocha())
+    // Creating the reports after tests ran
+    .pipe(istanbul.writeReports());
+});
 
 gulp.task('docs', function () {
   return gulp.src(SOURCE_CODE)
